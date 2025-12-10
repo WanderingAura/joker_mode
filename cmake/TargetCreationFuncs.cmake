4fdf8d2e513c78a09d340ec22ce894356f31529e
Jiachen Wang <jiachen.wang2000@gmail.com>
2025-12-10 21:36:27 +0000

    add debug-no-warning build

--- cmake/TargetCreationFuncs.cmake
+++ cmake/TargetCreationFuncs.cmake
@@ -1,14 +1,22 @@
 include_guard(DIRECTORY)
 
 set_property(DIRECTORY PROPERTY JOKER_COMPILER_FLAGS "-Wall;-Wextra;-Werror")
-set_property(DIRECTORY PROPERTY JOKER_MSVC_COMPILER_FLAGS "/W4;/WX;/permissive-")
+set_property(DIRECTORY PROPERTY JOKER_MSVC_COMPILER_FLAGS "/W4;/we4716;/we4715;/permissive-")
 
 function( _joker_set_options target )
     if (MSVC)
         get_directory_property(compiler_flags JOKER_MSVC_COMPILER_FLAGS)
+        if (NOT DEFINED JOKER_NO_WARNINGS)
+            list(APPEND compiler_flags "/WX")
+            message(STATUS ${compiler_flags})
+        endif()
     else()
         get_directory_property(compiler_flags JOKER_COMPILER_FLAGS)
+        if (NOT DEFINED JOKER_NO_WARNING)
+            list(APPEND compiler_flags "-Werror")
+        endif()
     endif()
+
 
     target_compile_options(${target} PRIVATE ${compiler_flags})
 
