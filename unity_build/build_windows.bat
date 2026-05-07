7afe7785afdfd3a855e8ff6210c342ab3e3eac70
Jiachen Wang <jiachen.wang2000@gmail.com>
2026-05-07 18:29:21 +0100

    add unity build support for exe

--- unity_build/build_windows.bat
+++ unity_build/build_windows.bat
@@ -12,10 +12,18 @@ setlocal enabledelayedexpansion
 
 pushd "%~dp0"
 
-echo Generating unity build...
 set "SRC_DIR=..\src"
-set "UNITY_FILE=soc_dll_unity_windows.c"
+set "UNITY_NAME=unity_windows"
+set "UNITY_FILE=%UNITY_NAME%.c"
 
+set "DLL_NAME=soc.dll"
+set "EXE_NAME=society.exe"
+
+del %UNITY_NAME%.c
+
+echo ====================================
+echo Generating %DLL_NAME% unity build...
+echo ====================================
 REM Normalize SRC_DIR to absolute form
 for %%a in ("%SRC_DIR%") do set "SRC_DIR=%%~fa"
 
@@ -55,7 +63,9 @@ for /d %%d in ("%SRC_DIR%\*") do (
 
 echo Done
 
-echo Compiling soc.dll unity build...
+echo ====================================
+echo Compiling %DLL_NAME% unity build...
+echo ====================================
 
 cl.exe !INCLUDES! ^
     /I"../build/debug/_deps/raylib-build/raylib/include" ^
@@ -66,17 +76,70 @@ cl.exe !INCLUDES! ^
     /W4 /we4716 /we4715 /wd4244 /wd4456 ^
     %UNITY_FILE% ^
     /link "../build/debug/_deps/raylib-build/raylib/raylib.lib" ^
-    /OUT:"../soc.dll"
+    /OUT:"../%DLL_NAME%"
 
 if %ERRORLEVEL% neq 0 (
     echo Build failed with error code %ERRORLEVEL%. Please check the logs above.
     exit /b %ERRORLEVEL%
 )
 
-del /Q soc_dll_unity_windows.exp 2>nul
-del /Q soc_dll_unity_windows.lib 2>nul
-del /Q soc_dll_unity_windows.obj 2>nul
-del /Q soc_dll_unity_windows.c 2>nul
+del /Q %UNITY_NAME%.exp 2>nul
+del /Q %UNITY_NAME%.lib 2>nul
+del /Q %UNITY_NAME%.obj 2>nul
+del /Q %UNITY_NAME%.c 2>nul
 
-echo Done
+echo ====================================
+echo Successfully compiled %DLL_NAME%
+echo ====================================
+
+if "%1" == "all" (
+  echo Generating %EXE_NAME% unity build...
+  set "VOS_DIR=..\src\vos"
+  for %%F in ("!VOS_DIR!\*.c") do (
+
+      :: Skip files containing "_linux"
+      echo %%~nxF | findstr /I "_linux" >nul
+      if errorlevel 1 (
+          >> "%UNITY_FILE%" echo #include "%%~fF"
+      )
+  )
+
+  set "BASED_DIR=..\src\based"
+  for %%F in ("!BASED_DIR!\*.c") do (
+
+      :: Skip files containing "_linux"
+      echo %%~nxF | findstr /I "_linux" >nul
+      if errorlevel 1 (
+          >> "%UNITY_FILE%" echo #include "%%~fF"
+      )
+  )
+
+  >> "%UNITY_FILE%" echo #include "..\src\app\main.c"
+
+  echo Done
+
+  echo ====================================
+  echo Compiling %EXE_NAME% unity build...
+  echo ====================================
+  cl.exe !INCLUDES! ^
+      /I"../build/debug/_deps/raylib-build/raylib/include" ^
+      /Zi ^
+      /std:c11 ^
+      /Fd"../" ^
+      /W4 /we4716 /we4715 /wd4244 /wd4456 ^
+      %UNITY_FILE% ^
+      /link "../build/debug/_deps/raylib-build/raylib/raylib.lib" ^
+      /OUT:"../%EXE_NAME%"
+
+  if %ERRORLEVEL% neq 0 (
+      echo Build failed with error code %ERRORLEVEL%. Please check the logs above.
+      exit /b %ERRORLEVEL%
+  )
+  del /Q %UNITY_NAME%.obj 2>nul
+  del /Q %UNITY_NAME%.c 2>nul
+  echo ====================================
+  echo Successfully compiled %EXE_NAME%
+  echo ====================================
+)
+
 popd
